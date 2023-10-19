691e303f1a0d74dd55aa83f2c4aa3df64e69e96e
tedroar <48267536+tedroar@users.noreply.github.com>
2023-10-19 21:04:34 +0800

    Update test_application.py

--- tests/test_application.py
+++ tests/test_application.py
@@ -13,4 +13,4 @@ def test_response(client):
     response_body = json.loads(result.get_data())
     assert result.status_code == 200
     assert result.headers['Content-Type'] == 'application/json'
-    assert response_body['Output'] == 'Hello World'
+    assert response_body['Output'] == 'Hello World from Codestar'
